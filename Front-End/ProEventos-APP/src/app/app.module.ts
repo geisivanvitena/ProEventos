d0f823cd73962c0eea7e0d97194db8696b6b1d79
Geisivan Vitena <gsv1205@yahoo.com>
2025-08-03 11:57:29 -0300

    feat(seção 5): Adiciona interpolação, diretivas e two-way data binding no Angular

--- Front-End/ProEventos-APP/src/app/app.module.ts
+++ Front-End/ProEventos-APP/src/app/app.module.ts
@@ -1,24 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'; // Importa o módulo responsável por permitir a realização de requisições HTTP
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Importa o módulo de animações do Angular
+
+// Importa o módulo responsável por permitir a realização de requisições HTTP
+import { HttpClientModule } from '@angular/common/http';
+// Importa o módulo de animações do Angular
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Importa os módulos necessários do ngx-bootstrap para funcionalidade de colapso (collapse)
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+// Importa o FormsModule para habilitar o uso de [(ngModel)] (two-way data binding) nos formulários
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
       EventosComponent,
-      PalestrantesComponent
+      PalestrantesComponent,
+      NavComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule 
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front-End/ProEventos-APP/src/app/eventos/eventos.component.ts
+++ Front-End/ProEventos-APP/src/app/eventos/eventos.component.ts
@@ -8,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  widthImg: number = 150;
+  marginImg: number = 2;
+  showImg: boolean = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { tema: string; local: string }) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+
+  }
 
   constructor( private http: HttpClient) { }
 
@@ -16,27 +39,20 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  alterarImagem() {
+    this.showImg = !this.showImg;
+  }
+
   public getEventos(): void {
 
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
     );
-    
-    this.eventos = [
-      {
-        Tema: 'Angular 10',
-        Local: 'São Paulo-SP'
-      },
-      {
-        Tema: '.NET 5',
-        Local: 'Salvador-BA'
-      },
-      {
-        Tema: 'Angular e suas novidades',
-        Local: 'Candeias-BA'
-      }
-    ]
+
   }
 
 }
